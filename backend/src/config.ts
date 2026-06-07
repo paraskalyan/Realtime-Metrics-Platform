d17860382536b18a612700c77869cc75c3570774
Paras Kalyan <paraskalyan20@gmail.com>
2026-06-07 23:59:13 +0530

    fix: correct default Redis URL to include port

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 export default {
     PORT: process.env.PORT || 4000,
-    REDIS_URL: process.env.REDIS_URL || 'redis://127.0.0.1',
+    REDIS_URL: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
     REDIS_PORT: process.env.REDIS_PORT || 6379,
 }
