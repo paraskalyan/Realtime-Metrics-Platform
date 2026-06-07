cd9981791d5955b0852704c7238fd0bb919a3f5e
Paras Kalyan <paraskalyan20@gmail.com>
2026-06-07 23:58:12 +0530

    feat: add Redis configuration options for URL and port

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,4 +3,6 @@ dotenv.config();
 
 export default {
     PORT: process.env.PORT || 4000,
+    REDIS_URL: process.env.REDIS_URL || 'redis://127.0.0.1',
+    REDIS_PORT: process.env.REDIS_PORT || 6379,
 }
