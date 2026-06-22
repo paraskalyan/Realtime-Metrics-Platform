29fe3795173abd32cc5e43f71d7d7a8bb69accf6
Paras Kalyan <paraskalyan20@gmail.com>
2026-06-22 23:59:02 +0530

    feat: add logout controller to auth module

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -7,3 +7,7 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
 export const login = async (req: Request, res: Response, next: NextFunction) => {
 
 }
+
+export const logout = async (req: Request, res: Response, next: NextFunction) => {
+
+}
