08b4866bef40f36b048bc2bca7eb3671f7a29b67
Paras Kalyan <paraskalyan20@gmail.com>
2026-06-25 23:56:49 +0530

    feat: implement logout functionality in auth controller

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,13 +1,34 @@
 import { NextFunction, Request, Response } from "express";
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
 
-}
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
+export const logout = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+    });
 
-}
-
-export const logout = async (req: Request, res: Response, next: NextFunction) => {
-
-}
+    return res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
